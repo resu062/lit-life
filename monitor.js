b17e3ebf60cbfc7e9f59084abed8078d0a87747a
resu062 <resu062@gmail.com>
2021-01-15 09:25:14 +0300

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -33,12 +33,12 @@ customElements.define('lit-monitor', class LitMonitor extends LitElement {
     firstUpdated() {
         super.firstUpdated();
         this.frame = 0;
-        this.startTime = Date.now();
+        this.startTime = performance.now();
         this.tick();
     }
 
     tick() {
-        let time = Date.now();
+        let time = performance.now();
         this.frame++;
         if (time - this.startTime > 1000) {
             this.fps = (this.frame / ((time - this.startTime) / 1000)).toFixed(1);
