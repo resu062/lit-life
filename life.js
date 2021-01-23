24cbe5c1c4e3988d7907843a1a0ea588bccbcb45
resu <63846162+resu062@users.noreply.github.com>
2021-01-23 13:03:06 +0300

    isc

--- life.js
+++ life.js
@@ -3,12 +3,13 @@ import { LitElement, html, css, svg } from 'https://cdn.pika.dev/lit-element';
 let ROWS, COLS, FRACTION, BOARD = [], _BOARD = [], first = true;
 const isAlive = ((row, col) => { return BOARD && BOARD[row] && BOARD[row][col] && BOARD[row][col].alive || false });
 const willLive = (row, col) => {
-    const  neighbours = isAlive(row - 1, col - 1) + isAlive(row - 1, col) + isAlive(row - 1, col + 1)
+    const neighbours = isAlive(row - 1, col - 1) + isAlive(row - 1, col) + isAlive(row - 1, col + 1)
         + isAlive(row, col - 1) + isAlive(row, col + 1)
         + isAlive(row + 1, col - 1) + isAlive(row + 1, col) + isAlive(row + 1, col + 1);
     return neighbours === 3 || neighbours === 2 && isAlive(row, col);
 }
-const generateBoardState = (liveFunc) => {
+const generateBoardState = (liveFunc = willLive) => {
+    if (first) liveFunc = () => { return Math.random() < FRACTION };
     _BOARD = [];
     for (let row = 0; row < ROWS; row++) {
         let newRow = []
@@ -23,7 +24,6 @@ const generateBoardState = (liveFunc) => {
             BOARD[row][col].hasAlive = BOARD[row][col].hasAlive || _BOARD[row][col].alive;
         }
 }
-const randomBoardState = () => { return generateBoardState(function () { return Math.random() < FRACTION }) }
 
 customElements.define('lit-life', class LitLife extends LitElement {
     static get properties() { return { rows: { type: Number }, cols: { type: Number }, size: { type: Number }, fraction: { type: Number } } }
@@ -34,7 +34,6 @@ customElements.define('lit-life', class LitLife extends LitElement {
         COLS = this.cols = 100;
         FRACTION = this.fraction = 0.2;
         this.size = 10;
-        randomBoardState();
     }
 
     static get styles() {
@@ -49,10 +48,10 @@ customElements.define('lit-life', class LitLife extends LitElement {
         return html`
             <svg width="${this.cols * this.size}" height="${this.rows * this.size}">
                 ${BOARD.map((row, r) =>
-                    svg`<g>
+            svg`<g>
                         ${row.map((col, c) => svg`<rect class="${col.alive ? 'alive' : ''} ${col.hasAlive ? 'haslive' : ''}" x="${c * this.size}" y="${r * this.size}" width="${this.size}" height="${this.size}"/>`)}
                     </g >`
-                )}
+        )}
             </svg>
         `;
     }
@@ -63,10 +62,10 @@ customElements.define('lit-life', class LitLife extends LitElement {
     }
 
     loop() {
-        setTimeout(() => {
-            requestAnimationFrame(() => this.loop());
-            this.requestUpdate()
-            generateBoardState(willLive);
-        }, 1000 / 30);
+        generateBoardState();
+        requestAnimationFrame(() => {
+            requestAnimationFrame(() => this.requestUpdate());
+            setTimeout(() => { requestAnimationFrame(() => this.loop()) }, 1000 / 60);
+        })
     }
-});
+})
